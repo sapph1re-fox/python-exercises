f2c55890b31393243588e79c4ccced2f408186ae
sapph1re-fox <magdalena.morongova@protonmail.com>
2024-11-26 12:02:54 +0100

    Add print exercise and rename the file

---
+++ print_exercise.py
@@ -0,0 +1,31 @@
+hello = "Hello"
+snake_variable = "snake"
+snake_list = ["snake1", "snake2", "snake3"]
+snake_dict = {"1": "snake1", "2": "snake2", "3": "snake3"}
+
+# print string
+print("Hello snake!")
+
+#print content of a variable
+print(snake_variable)
+
+#print addition of two numbers
+print(1+1)
+
+#print addition of two variables
+print(hello, snake_variable)
+print(hello + snake_variable)
+
+#print snake three times
+print(snake_variable * 3)
+
+#print list
+print(snake_list)
+
+#print first elemnt in the list
+print(snake_list[0])
+
+#print dictionary
+print(snake_dict)
+
+
